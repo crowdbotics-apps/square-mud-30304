994223123b622e040b7b9dadc29512930bbddacc
Crowdbotics <team@crowdbotics.com>
2021-09-03 23:17:24 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "square_mud_30304Identifier",
+  url: "https://square_mud_30304Identifier.botics.co",
+  api: "https://square_mud_30304Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
